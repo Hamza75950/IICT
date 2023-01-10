b41610ccb0af1e995080435fb770c7de8c6d04f0
Hamza75950 <122354114+Hamza75950@users.noreply.github.com>
2023-01-10 20:55:52 +0500

    Add files via upload

---
+++ hamza.py.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title("Biography")
+st.write("""This is the Biography of Hamza  
+
+    """)
+st.markdown("""# hobbies:
+    i like to read books
+    solve puzzels
+    like to play sports like badminton """)
+
+st.markdown(""" #Studend Info 
+name: Muhammad Ameer Hamza Khabn Niazi
+University: Karachi Institute Of Economic and Technology
+student ID: 65238   """)
+
